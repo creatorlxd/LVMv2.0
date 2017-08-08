4c4d7efe50f02c8045e74e83395bc3abcc60158f
creatorlxd <creatorlxd@outlook.com>
2017-08-08 20:58:30 +0800

    add base commandtype

--- LVMv2.0/LVM.cpp
+++ LVMv2.0/LVM.cpp
@@ -2,3 +2,23 @@
 #include "LVM.h"
 
 map<pair<CommandTypeType, int>, function<void(const vector<int>&)> > g_CommandRunner;
+
+void ThrowError(const string & error)
+{
+	cout << error << endl;
+	abort();
+}
+
+void RunCommand(CommandTypeType type, int size, const vector<int>& options)
+{
+	auto f = g_CommandRunner.find(make_pair(type, size));
+	if (f == g_CommandRunner.end())
+	{
+		ThrowError("do not have this command type");
+		return;
+	}
+	else
+	{
+		(*f).second(options);
+	}
+}
--- LVMv2.0/LVM.h
+++ LVMv2.0/LVM.h
@@ -10,10 +10,14 @@ extern map<pair<CommandTypeType, int>, function<void(const vector<int>&)> > g_Co
 inline str(){g_CommandRunner.insert(make_pair(make_pair(str::type,str::size),str))}
 
 template<CommandTypeType type,int size>
-class BaseCommand
+class BaseCommandType
 {
 public:
-	REGISTERCOMMAND(BaseCommand);
+	REGISTERCOMMAND(BaseCommandType);
 	static const CommandTypeType m_Type = type;
 	static const int m_Size = size;
 };
+
+void ThrowError(const string& error);
+
+void RunCommand(CommandTypeType type, int size,const vector<int>& options);
