18aed483fb3e5c4d87df4408bf794c1d4b058a54
Xiaode Li <1461637345@qq.com>
2017-09-05 17:50:50 +0800

    Update main.cpp

--- LVMv2.0/main.cpp
+++ LVMv2.0/main.cpp
@@ -5,25 +5,21 @@
 
 int main()
 {
+	string filename;
+	if (argc == 2)
+	{
+		filename = argv[1];
+	}
+	else
+	{
+		cin >> filename;
+	}
 	LVMRunner System;
 	ExternLibray SystemLibray = { make_pair([&](LVMRunner& runner,const vector<int>& options)->void {
 		system("pause");
 	},0) };
 	System.m_ExternLibrary.push_back(SystemLibray);
-	System.m_CommandReader.InitFromMemory(vector<Command>{
-		Command{4,{1024,10}},			// asc(10) == \n
-		Command{ 2,{8,76}},
-		Command{2,{0,0}},
-		Command{2,{4,0}},
-		Command{5,{0}},
-		Command{5,{4}},
-		Command{54,{0,4,4}},
-		Command{48,{8,9,1}},
-		Command{10,{8} },
-		Command{},
-		Command{10,{1024}},
-		Command{55,{0,0,16}}
-	});
+	System.InitFromFile(filename);
 	System.Run();
 	system("pause");
 	return 0;
